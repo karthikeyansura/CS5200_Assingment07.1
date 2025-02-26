febbbfb7be8da2a5adc93d345567e5149c951731
karthikeyan sura <surakarthikeyan.2002@gmail.com>
2025-02-25 20:18:26 -0500

    Third Commit

--- SuraS.CS5200.Trigger.R
+++ SuraS.CS5200.Trigger.R
@@ -2,24 +2,34 @@
 # Author: "Sai Karthikeyan, Sura"
 # Date: "02/25/2025"
 
-# Step 1: Load RSQLite library
-library(RSQLite)
-
-# Step 2: Connect to the SQLite database
-dbCon <- dbConnect(RSQLite::SQLite(), "OrdersDB.sqlitedb.db")
-
-# Function to check if a column exists in the Employees table
-column_exists <- function(column_name) {
-  columns <- dbGetQuery(dbCon, "PRAGMA table_info(Employees);")
-  return(column_name %in% columns$name)
+# Load required libraries
+installPackages <- function(packages) {
+  # install packages not yet installed
+  installed_packages <- packages %in% rownames(installed.packages())
+  if (any(installed_packages == FALSE)) {
+    install.packages(packages[!installed_packages])
+  }
+  # load all packages by applying 'library' function
+  invisible(lapply(packages, library, character.only = TRUE))
 }
 
-# Step 3: Alter the Employees table to add the TotalSold column
-if (!column_exists("TotalSold")) {
-  dbExecute(dbCon, "ALTER TABLE Employees ADD COLUMN TotalSold NUMERIC CHECK (TotalSold >= 0);")
+# Connect to the SQLite database
+ConnectSQLiteDB <- function(dbName) {
+  dbFile = paste0(dbName)
+  dbCon <- dbConnect(RSQLite::SQLite(), dbFile)
+  return(dbCon)
 }
 
-# Step 4: Update TotalSold column with the total revenue per employee
+# Alter the Employees table to add the TotalSold column
+AddTotalSoldInEmployeesTable <- function(dbCon) {
+if ("TotalSold" %in% dbListFields(dbCon, "Employees")) {
+  dbExecute(dbCon, "ALTER TABLE Employees DROP COLUMN TotalSold;")
+}
+dbExecute(dbCon, "ALTER TABLE Employees ADD COLUMN TotalSold NUMERIC;")
+}
+
+# Update TotalSold column with the total revenue per employee
+updateTotalSoldColumnInEmployeesTable <- function(dbCon) {
 dbExecute(dbCon, "UPDATE Employees
 SET TotalSold = COALESCE((
     SELECT SUM(od.Quantity * p.Price)
@@ -28,15 +38,11 @@ SET TotalSold = COALESCE((
     JOIN Products p ON od.ProductID = p.ProductID
     WHERE o.EmployeeID = Employees.EmployeeID
 ), 0);")
-
-# Function to check if the trigger exists in the sqlite_master table
-trigger_exists <- function(trigger_name) {
-  result <- dbGetQuery(dbCon, sprintf("SELECT name FROM sqlite_master WHERE type='trigger' AND name='%s';", trigger_name))
-  return(nrow(result) > 0)
 }
 
-# Step 5: Create an AFTER INSERT on OrderDetails
-if (!trigger_exists("update_totalsold_after_insert")) {
+# Create an AFTER INSERT on OrderDetails
+insertTriggerOnOrderDetailsTable <- function(dbCon) {
+  dbExecute(dbCon, "DROP TRIGGER IF EXISTS update_totalsold_after_insert;")
   dbExecute(dbCon, "CREATE TRIGGER update_totalsold_after_insert
   AFTER INSERT ON OrderDetails
   FOR EACH ROW
@@ -55,8 +61,9 @@ if (!trigger_exists("update_totalsold_after_insert")) {
   END;")
 }
 
-# Step 6: Create an AFTER UPDATE trigger on OrderDetails
-if (!trigger_exists("update_totalsold_after_update")) {
+# Create an AFTER UPDATE trigger on OrderDetails
+updateTriggerOnOrderDetailsTable <- function(dbCon) {
+  dbExecute(dbCon, "DROP TRIGGER IF EXISTS update_totalsold_after_update;")
   dbExecute(dbCon, "CREATE TRIGGER update_totalsold_after_update
   AFTER UPDATE ON OrderDetails
   FOR EACH ROW
@@ -75,8 +82,9 @@ if (!trigger_exists("update_totalsold_after_update")) {
   END;")
 }
 
-# Step 7: Create an AFTER DELETE trigger on OrderDetails
-if (!trigger_exists("update_totalsold_after_delete")) {
+# Create an AFTER DELETE trigger on OrderDetails
+deleteTriggerOnOrderDetailsTable <- function(dbCon) {
+  dbExecute(dbCon, "DROP TRIGGER IF EXISTS update_totalsold_after_delete;")
   dbExecute(dbCon, "CREATE TRIGGER update_totalsold_after_delete
   AFTER DELETE ON OrderDetails
   FOR EACH ROW
@@ -95,5 +103,148 @@ if (!trigger_exists("update_totalsold_after_delete")) {
   END;")
 }
 
-# Step 8: Disconnect from the database
-dbDisconnect(dbCon)
+# Validating the Triggers
+validateInsertTrigger <- function(dbCon) {
+  tryCatch({
+    cat("Validating insert trigger\n")
+    # sample data for validation
+    employeeIdToTest <- 3
+    productIdToTest <- 3
+    orderIdToTest <- 10253
+    
+    # get the total sold of employee before inserting a row in OrderDetails
+    employee <- dbGetQuery(dbCon, sprintf("SELECT * FROM Employees where EmployeeID = %s;", employeeIdToTest))
+    cat(sprintf("Total sold by the employee with the id %s is %s\n", employeeIdToTest, employee$TotalSold))
+    
+    # get the price of the product
+    productPrice <- dbGetQuery(dbCon, sprintf("SELECT Price FROM Products where ProductID = %s;", productIdToTest))
+    cat(sprintf("Price of the product for 1 unit is %s\n", productPrice$Price))
+    
+    # insert a row in OrderDetails with quantity 1
+    dbExecute(dbCon, sprintf("INSERT INTO OrderDetails (OrderID, ProductID, Quantity) VALUES (%s, %s, 1);", orderIdToTest, productIdToTest))
+    
+    # get the total sold of employee after inserting a row in OrderDetails
+    employeeAfterInsert <- dbGetQuery(dbCon, sprintf("SELECT * FROM Employees where EmployeeID = %s;", employeeIdToTest))
+    cat(sprintf("Total sold by the employee with the id %s after inserting 1 quantity is %s\n", employeeIdToTest, employeeAfterInsert$TotalSold))
+    
+    # validate the trigger by checking if the TotalSold column in Employees table is updated
+    cat(sprintf("Expected increase in price: %s, actual:%s\n", productPrice$Price * 1, employeeAfterInsert$TotalSold - employee$TotalSold))
+    test_that("Test Insert Trigger:", {
+      expect_equal(employeeAfterInsert$TotalSold - employee$TotalSold, productPrice$Price * 1)
+    })
+    cat("\n")
+  }, error = function(e) {
+    cat("Error while validating insert trigger: ", e$message, "\n")
+  })
+}
+
+
+validateUpdateTrigger <- function(dbCon) {
+  tryCatch({
+    cat("Validating update trigger\n")
+    # sample data for validation
+    employeeIdToTest <- 3
+    productIdToTest <- 3
+    orderIdToTest <- 10253
+    
+    # get the total sold of employee before updating a row in OrderDetails
+    employee <- dbGetQuery(dbCon, sprintf("SELECT * FROM Employees where EmployeeID = %s;", employeeIdToTest))
+    cat(sprintf("Total sold by the employee with the id %s is %s\n", employeeIdToTest, employee$TotalSold))
+    
+    # get the price of the product
+    productPrice <- dbGetQuery(dbCon, sprintf("SELECT Price FROM Products where ProductID = %s;", productIdToTest))
+    cat(sprintf("Price of the product for 1 unit is %s\n", productPrice$Price))
+    
+    # update the quantity of a row in OrderDetails to 2
+    dbExecute(dbCon, sprintf("UPDATE OrderDetails SET Quantity = 2 WHERE OrderID = %s AND ProductID = %s;", orderIdToTest, productIdToTest))
+    
+    # get the total sold of employee after updating a row in OrderDetails
+    employeeAfterUpdate <- dbGetQuery(dbCon, sprintf("SELECT * FROM Employees where EmployeeID = %s;", employeeIdToTest))
+    cat(sprintf("Total sold by the employee with the id %s after updating quantity to 2 is %s\n", employeeIdToTest, employeeAfterUpdate$TotalSold))
+
+    # validate the trigger by checking if the TotalSold column in Employees table is updated
+    cat(sprintf("Expected increase in price: %s, actual:%s\n", productPrice$Price * 1, employeeAfterUpdate$TotalSold - employee$TotalSold))
+    
+    # assert the expected and actual values
+    test_that("Test Update Trigger:", {
+      expect_equal(employeeAfterUpdate$TotalSold - employee$TotalSold, productPrice$Price * 1)
+    })
+    cat("\n")
+  }, error = function(e) {
+    cat("Error while validating update trigger: ", e$message, "\n")
+  })
+}
+
+validateDeleteTrigger <- function(dbCon) {
+  tryCatch({
+    cat("Validating delete trigger\n")
+    # sample data for validation
+    employeeIdToTest <- 3
+    productIdToTest <- 3
+    orderIdToTest <- 10253
+    
+    # get the total sold of employee before deleting a row in OrderDetails
+    employee <- dbGetQuery(dbCon, sprintf("SELECT * FROM Employees where EmployeeID = %s;", employeeIdToTest))
+    cat(sprintf("Total sold by the employee with the id %s is %s\n", employeeIdToTest, employee$TotalSold))
+    
+    # get the price of the product
+    productPrice <- dbGetQuery(dbCon, sprintf("SELECT Price FROM Products where ProductID = %s;", productIdToTest))
+    cat(sprintf("Price of the product for 1 unit is %s\n", productPrice$Price))
+    
+    # delete the row in OrderDetails
+    dbExecute(dbCon, sprintf("DELETE FROM OrderDetails WHERE OrderID = %s AND ProductID = %s;", orderIdToTest, productIdToTest))
+    
+    # get the total sold of employee after deleting a row in OrderDetails
+    employeeAfterDelete <- dbGetQuery(dbCon, sprintf("SELECT * FROM Employees where EmployeeID = %s;", employeeIdToTest))
+    cat(sprintf("Total sold by the employee with the id %s after deleting quantity is %s\n", employeeIdToTest, employeeAfterDelete$TotalSold))
+    
+    # validate the trigger by checking if the TotalSold column in Employees table is updated
+    cat(sprintf("Expected decrease in price: %s, actual:%s\n", productPrice$Price * 2, employee$TotalSold - employeeAfterDelete$TotalSold))
+    
+    # assert the expected and actual values
+    test_that("Test Delete Trigger:", {
+      expect_equal(employee$TotalSold - employeeAfterDelete$TotalSold, productPrice$Price * 2)
+    })
+    cat("\n")
+  }, error = function(e) {
+    cat("Error while validating delete trigger: ", e$message, "\n")
+  })
+}
+
+validateTriggers <- function(dbCon) {
+  # validate the insert trigger
+  validateInsertTrigger(dbCon)
+  # validate the update trigger
+  validateUpdateTrigger(dbCon)
+  # validate the delete trigger
+  validateDeleteTrigger(dbCon)
+}
+
+# Main function
+main <- function() {
+  # Step 1: Load required libraries
+  installPackages(c("RSQLite", "testthat"))
+  
+  # Step 2: Connect to the SQLite database
+  dbCon <- ConnectSQLiteDB("OrdersDB.sqlitedb.db")
+  
+  # Step 3: Alter the Employees table and add the TotalSold column
+  AddTotalSoldInEmployeesTable(dbCon)
+  
+  # Step 4: Update the TotalSold column with the total revenue for each employee
+  updateTotalSoldColumnInEmployeesTable(dbCon)
+  
+  # Step 5: Create the triggers
+  insertTriggerOnOrderDetailsTable(dbCon)
+  updateTriggerOnOrderDetailsTable(dbCon)
+  deleteTriggerOnOrderDetailsTable(dbCon)
+  
+  # Step 6: validate the triggers
+  validateTriggers(dbCon)
+  
+  # Step 7: Disconnect from the database
+  dbDisconnect(dbCon)
+}
+
+# Run the main function
+main()
